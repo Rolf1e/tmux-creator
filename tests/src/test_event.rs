c40b44f1b3f7762c62faad1aa0ba3ba78c81ba4f
Rolf1e <tigran.slama@laposte.net>
2021-04-20 22:16:06 +0200

    fix tests

--- tests/src/test_event.rs
+++ tests/src/test_event.rs
@@ -10,7 +10,7 @@ pub mod test_event {
     fn should_not_interprete_neovim_event() {
         let event_handler = event_handler::EventHandler::default();
         if let event::EventResponse::Exception(exception::NeovimException::UnknowMessage(message)) =
-            event_handler.handle_event(String::from("bad-event"), Vec::new())
+            event_handler.handle_event(String::from("bad-event"), Vec::new(), (212, 100))
         {
             assert_eq!(String::from("bad-event"), message);
         } else {
